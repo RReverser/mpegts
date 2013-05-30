5f4f2a9a7ee7f6b63caa078be9cd3187c798d0e9
Ingvar Stepanyan <me@rreverser.com>
2013-05-30 21:04:16 +0300

    Added few more samples; now creating array of samples with corresponding data (offset in mdat, PTS, DTS, is IDR flag).

---
+++ pes.js
@@ -0,0 +1,78 @@
+(function (exports) {
+
+var PES = {
+	Flag: jBinary.Property(
+		['dependentField'],
+		function () {
+			return this.binary.read(1);
+		},
+		function () {
+			this.binary.write(1, (this.dependentField in this.binary.getContext() ? 1 : 0));
+		}
+	),
+
+	FlagDependent: jBinary.Property(
+		['flagField', 'baseType'],
+		function () {
+			return this.binary.read(['if', this.flagField, this.baseType]);
+		},
+		function () {
+			this.binary.write(this.baseType);
+		}
+	),
+
+	PESTimeStamp: jBinary.Property(
+		function (prefix) {
+			var skipBit = ['const', 1, 1, true];
+			this.baseType = {
+				_prefix: ['const', 4, prefix, true],
+				hiPart: 3,
+				_skip1: skipBit,
+				midPart: 15,
+				_skip2: skipBit,
+				loPart: 15,
+				_skip3: skipBit
+			};
+		},
+		function () {
+			var parts = this.binary.read(this.baseType);
+			return parts.loPart | (parts.midPart << 15) | (parts.hiPart << 30);
+		},
+		function (value) {
+			this.binary.write(this.baseType, {
+				hiPart: value >>> 30,
+				midPart: (value >>> 15) & ~(-1 << 15),
+				loPart: value & ~(-1 << 15)
+			});
+		}
+	),
+
+	PESHeader: {
+		length: 'uint16',
+		_marker: ['const', 2, 2],
+		scramblingControl: ['enum', 2, ['not_scrambled']],
+		priority: 1,
+		dataAlignmentIndicator: 1,
+		hasCopyright: 1,
+		isOriginal: 1,
+		_hasPTS: ['Flag', 'pts'],
+		_hasDTS: ['Flag', 'dts'],
+		_hasESCR: ['Flag', 'escr'],
+		_hasESRate: ['Flag', 'esRate'],
+		dsmTrickMode: 1,
+		_hasAdditionalCopyInfo: ['Flag', 'additionalCopyInfo'],
+		_hasPESCRC: ['Flag', 'pesCRC'],
+		_hasExtension: ['Flag', 'extension'],
+		dataLength: 'uint8',
+		pts: ['FlagDependent', '_hasPTS', ['if', ['_hasDTS'], ['PESTimeStamp', 3], ['PESTimeStamp', 2]]],
+		dts: ['FlagDependent', '_hasDTS', ['PESTimeStamp', 1]]
+	}
+};
+
+if (typeof module !== 'undefined' && exports === module.exports) {
+	module.exports = PES;
+} else {
+	exports.PES = PES;
+}
+
+})(this);
